d370a8f3627bf681812d3ce8b9e9acb5a8cb0908
[jli30] <[jain.li@nasa.gov]>
2025-02-19 22:04:20 -0500

    write output to npz

--- sample_call.py
+++ sample_call.py
@@ -1,5 +1,6 @@
 from processor import Processor
 import xarray as xr
+import numpy as np
 def main():
     # Load sample data and call the predict function
     # This part can be replaced with any data source
@@ -10,18 +11,19 @@ def main():
     ## Sample data load from a netcdf file
     ds = xr.open_dataset("/discover/nobackup/jli30/data/geos_prog/REPLAY_M2-100KM-L181-AMIP-GFDL.geosgcm_prog.20021230_1500z.nc4")
     variables = ['U', 'V', 'T', 'QV', 'QI', 'QL', 'QG', 'QR', 'QS', 'PS']
-    arrays = [ds[var].to_numpy().squeeze for var in variables]
+    arrays = [ds[var].to_numpy().squeeze() for var in variables]
 
     ## Another argument needed is the path to the checkpoint directory
     # ckpt_root_path = "/path/to/checkpoint/directory"
     # If the path is static, you can hardcode it in the Processor class
-    ckpt_root_path = "/discover/nobackup/jli30/data/geos_prog/ckpt" 
+    #ckpt_root_path = "/discover/nobackup/jli30/data/geos_prog/ckpt" 
+    ckpt_root_path = "/discover/nobackup/jli30/geos_state/SmaAt-UNet/geos_state_bias/checkpoints/batch_2"
     ############################################################    
     
     # Processor class is responsible for making predictions
-    processor = Processor(ckpt_root_path=ckpt_root_path, *arrays)
+    processor = Processor(ckpt_root_path, *arrays)
     outputs = processor.predict()
-    print(outputs.shape)
+    np.savez("sample_out.npz", *outputs)
 
 if __name__ == "__main__":
     main()
